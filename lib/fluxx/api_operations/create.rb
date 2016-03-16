ba8b98c65dae84fa4b743668e0b78aa0ecfb3c9a
Alex Naser <c.a.naser@gmail.com>
2016-03-16 10:58:17 -0400

    fix to send creates without existing @opts

--- lib/fluxx/api_operations/create.rb
+++ lib/fluxx/api_operations/create.rb
@@ -2,7 +2,7 @@ module Fluxx
   module ApiOperations
     module Create
       def create(attrs, opts = {})
-        opts = opts.merge(@opts)
+        opts = opts.merge(@opts) if @opts
         response = request :create, data: attrs, options: opts
         ApiResource.construct_from @model_type, response[@model_type], opts
       end
