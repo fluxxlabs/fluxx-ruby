933bc4f0ea6010dab94cb6cf78a3e9df3f000d2d
Alex Naser <c.a.naser@gmail.com>
2016-03-17 19:55:39 -0400

    better naming in List::list

--- lib/fluxx/api_operations/list.rb
+++ lib/fluxx/api_operations/list.rb
@@ -4,8 +4,8 @@ module Fluxx
       def list(opts = {})
         response = request :list, options: opts
         model_type, resp_objects = response.delete('records').first
-        response_opts = Util.symbolize_names(opts).merge Util.symbolize_names(response)
-        ListObject.construct_from(model_type, { data: resp_objects }, response_opts)
+        resp_opts = Util.symbolize_names(opts).merge Util.symbolize_names(response)
+        ListObject.construct_from(model_type, { data: resp_objects }, resp_opts)
       end
 
       def all(opts = {})
