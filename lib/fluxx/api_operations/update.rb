1649a9c5c5d330cc38031db07a3465fe4b7358b9
Alex Naser <c.a.naser@gmail.com>
2016-03-17 19:55:59 -0400

    aliasing update for the rails 3.2'ers

--- lib/fluxx/api_operations/update.rb
+++ lib/fluxx/api_operations/update.rb
@@ -20,6 +20,8 @@ module Fluxx
         response = self.request :update, model_id: @values[:id], data: values, options: opts
         self.reset_from response[@model_type], opts
       end
+
+      alias_method :update_attributes, :update
     end
   end
 end
