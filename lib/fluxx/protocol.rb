78a3cded4fa9f9b84935b23b4d255300cbbe2aa7
Alex Naser <c.a.naser@gmail.com>
2016-03-21 17:33:10 -0400

    cleaning out cruft

--- lib/fluxx/protocol.rb
+++ lib/fluxx/protocol.rb
@@ -1,9 +1,3 @@
 module Fluxx
-  class Protocol
-    ApiService = DRbObject.new_with_uri('druby://localhost:8788')
-
-    def self.get(url, headers)
-      ApiService.index(model_type: url)
-    end
-  end
+  class Protocol; end
 end
